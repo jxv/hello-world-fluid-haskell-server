5791b0ba8536452450dd619b190a320e1b67eeda
jxv <joevargas92@gmail.com>
2018-03-22 19:48:39 -0700

    version 1.0

--- library/Api/Server.hs
+++ library/Api/Server.hs
@@ -17,17 +17,29 @@ module Api.Server
   , helloWorld'spec
   , helloWorld'Scotty'Post
   , helloWorld'Scotty'Get
-  , V0.Hello(..)
-  , V0.Goodbye(..)
-  , V0.HelloWorld'Service(..)
-  , V0.HelloWorld'Thrower(..)
-  , V0.helloWorld'pull
+  , V1.Hello(..)
+  , V1.Goodbye(..)
+  , V1.Lang(..)
+  , V1.HelloWorld'Service(..)
+  , V1.HelloWorld'Thrower(..)
+  , V1.helloWorld'pull
   ) where
 
 import qualified Prelude as P
 import qualified Fluid.Server as C (RuntimeThrower, Hooks, Request, Response, Major, Minor, Pull)
 import qualified Fluid.Imports as R
 import qualified Fluid.Server.Scotty as Scotty
+import qualified Api.Major1 as V1
+  ( HelloWorld'Service(..)
+  , HelloWorld'Thrower(..)
+  , helloWorld'handler
+  , helloWorld'version
+  , helloWorld'pull
+  , helloWorld'spec
+  , Hello(..)
+  , Goodbye(..)
+  , Lang(..)
+  )
 import qualified Api.Major0 as V0
   ( HelloWorld'Service(..)
   , HelloWorld'Thrower(..)
@@ -43,18 +55,22 @@ helloWorld'handlerMap
   ::
     ( R.MonadIO m
     , R.MonadCatch m
+    , V1.HelloWorld'Service meta1 m
     , V0.HelloWorld'Service meta0 m
     )
-  => (xtra -> C.Hooks m () meta0)
+  => (xtra -> C.Hooks m () meta1)
+  -> (xtra -> C.Hooks m () meta0)
   -> xtra
   -> R.Map C.Major (C.Minor, C.Request -> m (P.Either C.Response C.Response))
-helloWorld'handlerMap hooks0 xtra = R.fromList
-    [ (0, (1, V0.helloWorld'handler hooks0 xtra))
+helloWorld'handlerMap hooks1 hooks0 xtra = R.fromList
+    [ (1, (0, V1.helloWorld'handler hooks1 xtra))
+    , (0, (1, V0.helloWorld'handler hooks0 xtra))
     ]
 
 helloWorld'spec :: R.Value
 helloWorld'spec = R.toJSON
-  [ V0.helloWorld'spec
+  [ V1.helloWorld'spec
+  , V0.helloWorld'spec
   ]
 
 helloWorld'Scotty'Post
@@ -62,12 +78,14 @@ helloWorld'Scotty'Post
     ( Scotty.ScottyError e
     , R.MonadIO m
     , R.MonadCatch m
+    , V1.HelloWorld'Service meta1 m
     , V0.HelloWorld'Service meta0 m
     )
   => C.Pull
+  -> ([(Scotty.LazyText, Scotty.LazyText)] -> C.Hooks m () meta1)
   -> ([(Scotty.LazyText, Scotty.LazyText)] -> C.Hooks m () meta0)
   -> Scotty.ScottyT e m ()
-helloWorld'Scotty'Post pull hooks0 = Scotty.respond pull (helloWorld'handlerMap hooks0)
+helloWorld'Scotty'Post pull hooks1 hooks0 = Scotty.respond pull (helloWorld'handlerMap hooks1 hooks0)
 
 helloWorld'Scotty'Get :: (Scotty.ScottyError e, R.MonadIO m) => C.Pull -> Scotty.ScottyT e m ()
 helloWorld'Scotty'Get = Scotty.getSpec helloWorld'spec
--- library/HelloWorld.hs
+++ library/HelloWorld.hs
@@ -9,27 +9,39 @@ import Fluid.Server
 
 import Api.Server
 import qualified Api.Major0 as V0
+import qualified Api.Major1 as V1
 
 newtype App a = App { unApp :: IO a }
   deriving (Functor, Applicative, Monad, MonadIO, MonadCatch, MonadThrow)
 
 instance ServiceThrower App
 instance V0.HelloWorld'Thrower App
+instance V1.HelloWorld'Thrower App
 
 instance V0.HelloWorld'Service () App where
   helloWorld'Hello () = hello
   helloWorld'Goodbye () = goodbye
 
+instance V1.HelloWorld'Service () App where
+  helloWorld'Hello () = hello'
+  helloWorld'Goodbye () = goodbye
+
 hello :: Monad m => V0.Hello -> m Text
-hello V0.Hello{helloWho} = return $ "Hello " `mappend` helloWho `mappend` "!"
+hello req = return $ "Hello " `mappend` (V0.helloWho req) `mappend` "!"
+
+hello' :: Monad m => V1.Hello -> m Text
+hello' req = case V1.helloLang req of
+  V1.Lang'English -> return $ "Hello " `mappend` (V1.helloWho req) `mappend` "!"
+  V1.Lang'Spanish -> return $ "Hola " `mappend` (V1.helloWho req) `mappend` "!"
+  V1.Lang'French -> return $ "Bonjour " `mappend` (V1.helloWho req) `mappend` "!"
 
 goodbye :: Monad m => V0.Goodbye -> m Text
-goodbye V0.Goodbye{goodbyeWho} = return $ "Goodbye " `mappend` goodbyeWho `mappend` "!"
+goodbye req = return $ "Goodbye " `mappend` (V0.goodbyeWho req) `mappend` "!"
 
 main :: IO ()
 main = runServer helloWorld'pull unApp routes
 
 routes :: ScottyT TL.Text App ()
 routes = do
-  helloWorld'Scotty'Post helloWorld'pull (const defHooks)
+  helloWorld'Scotty'Post helloWorld'pull (const defHooks) (const defHooks)
   helloWorld'Scotty'Get helloWorld'pull
